2ba2dcfa0449f36025f2ecc69b424357d4cdae21
vishal <vishalpandhare2016@gmail.com>
2022-12-06 14:57:15 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+
+const route = require('./routes/route.js');
+
+const app = express();
+
+const multer= require("multer");
+const { AppConfig } = require('aws-sdk');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use( multer().any())
+
+app.use('/', route);
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
